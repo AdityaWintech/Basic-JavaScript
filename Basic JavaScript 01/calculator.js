be664afeefafa047b036c22aa4d737de9ad36682
Aditya Kumar <123807884+AdityaWintech@users.noreply.github.com>
2023-03-06 23:08:51 +0530

    Add files via upload

---
+++ Basic JavaScript 01/calculator.js
@@ -0,0 +1,24 @@
+var first_number;
+first_number=45;
+var second_number;
+second_number=46;
+var symbol;
+symbol='*';
+switch(symbol){
+        case'+':
+        console.log(first_number+second_number);
+        break;
+        case'*':
+        console.log(first_number*second_number);
+        break;
+        case'/':
+        console.log(first_number/second_number);
+        break;
+        case'-':
+        console.log(first_number-second_number);
+        break;
+        case'%':
+        console.log(first_number%second_number);
+        break;
+
+}
